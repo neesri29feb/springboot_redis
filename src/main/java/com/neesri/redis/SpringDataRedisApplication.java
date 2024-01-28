a27ea70a289f67fe177025195282db831657699f
neesri <neesri29feb@gmail.com>
2024-01-28 15:31:50 +0530

    Revert "redis"
    This reverts commit 51d84ac798c5e589c8d3b5b37e3f97a1fe912aa6.

--- src/main/java/com/neesri/redis/SpringDataRedisApplication.java
+++ src/main/java/com/neesri/redis/SpringDataRedisApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDataRedisApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Spring data redis example ------------------------------");
+		System.out.println("Spring data redis example --------------------------");
 		SpringApplication.run(SpringDataRedisApplication.class, args);
 	}
 
